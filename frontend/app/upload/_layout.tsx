52dd50592741c502e67f231ca184180492db0e25
57barkat <barkatullahkhan120136@gmail.com>
2025-10-27 14:15:25 -0400

    wrapped with form provider

--- frontend/app/upload/_layout.tsx
+++ frontend/app/upload/_layout.tsx
@@ -1,23 +1,26 @@
+import React from "react";
+import { FormProvider } from "@/contextStore/FormContext";
 import { Stack } from "expo-router";
 
 export default function UploadLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {/* <Stack.Screen name="index"/>           */}
-      <Stack.Screen name="IntroStep1" />    
-      <Stack.Screen name="Location" />   
-      <Stack.Screen name="CreateStep" />  
-      <Stack.Screen name="PropertyDetails" />    
-      <Stack.Screen name="AmenitiesScreen" />    
-      <Stack.Screen name="PhotosScreen" />    
-      <Stack.Screen name="ListingTitleScreen" />    
-      <Stack.Screen name="ListingDescriptionHighlightsScreen" />    
-      <Stack.Screen name="FinalDescriptionScreen" />    
-      <Stack.Screen name="BookingSettingsScreen" />    
-      <Stack.Screen name="PricingScreen" />    
-      <Stack.Screen name="WeekendPricingScreen" />    
-      <Stack.Screen name="SafetyDetailsScreen" />    
-      <Stack.Screen name="FinalAddressDetailsScreen" />    
-    </Stack>
+    <FormProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="IntroStep1" />
+        <Stack.Screen name="Location" />
+        <Stack.Screen name="CreateStep" />
+        <Stack.Screen name="PropertyDetails" />
+        <Stack.Screen name="AmenitiesScreen" />
+        <Stack.Screen name="PhotosScreen" />
+        <Stack.Screen name="ListingTitleScreen" />
+        <Stack.Screen name="ListingDescriptionHighlightsScreen" />
+        <Stack.Screen name="FinalDescriptionScreen" />
+        <Stack.Screen name="BookingSettingsScreen" />
+        <Stack.Screen name="PricingScreen" />
+        <Stack.Screen name="WeekendPricingScreen" />
+        <Stack.Screen name="SafetyDetailsScreen" />
+        <Stack.Screen name="FinalAddressDetailsScreen" />
+      </Stack>
+    </FormProvider>
   );
 }
