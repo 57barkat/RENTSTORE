7120fbe99cbad31f7dbd5aa9b27f59f9a5898fa5
57barkat <barkatullahkhan120136@gmail.com>
2025-10-28 09:08:04 -0400

    Added dark and light theme feature.

--- frontend/app/(tabs)/upload.tsx
+++ frontend/app/(tabs)/upload.tsx
@@ -1,39 +1,53 @@
 import React, { FC } from "react";
 import { Text, View } from "react-native";
 import { useRouter } from "expo-router";
-import { styles } from "@/styles/CreateStep";
 import { OptionCard } from "@/components/UploadPropertyComponents/OptionCard";
-
-
+import { useTheme } from "@/contextStore/ThemeContext";
+import { Colors } from "@/constants/Colors";
+import { styles } from "@/styles/CreateStep";
 
 const CreateStep: FC = () => {
   const router = useRouter();
+  const { theme } = useTheme();
+  const currentTheme = Colors[theme ?? "light"];
 
   const handleCreateNew = () => {
-    // router.push(NEW_LISTING_PATH as `${string}:param`);
     router.push("/upload/IntroStep1");
   };
 
   const handleCreateFromExisting = () => {
     router.push("/DraftProperties" as `${string}:param`);
-
     console.log("Navigating to existing listings selection.");
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerTitle}>Start a new listing</Text>
+    <View
+      style={[styles.container, { backgroundColor: currentTheme.background }]}
+    >
+      <Text style={[styles.headerTitle, { color: currentTheme.text }]}>
+        Start a new listing
+      </Text>
 
       <OptionCard
         iconName="home-plus-outline"
         title="Create a new listing"
         onPress={handleCreateNew}
+        backgroundColor={currentTheme.card}
+        iconColor={currentTheme.primary}
+        textColor={currentTheme.text}
       />
-      <View style={styles.separator} />
+
+      <View
+        style={[styles.separator, { backgroundColor: currentTheme.border }]}
+      />
+
       <OptionCard
         iconName="content-copy"
         title="Create from an existing listing"
         onPress={handleCreateFromExisting}
+        backgroundColor={currentTheme.card}
+        iconColor={currentTheme.primary}
+        textColor={currentTheme.text}
       />
     </View>
   );
