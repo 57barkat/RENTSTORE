dbcdde606a6136261d0349a95ced9e6601e8635d
57barkat <barkatullahkhan120136@gmail.com>
2025-11-19 13:10:52 -0500

    style: standardize quotes and improve variable declaration for PORT in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true }); 
-  app.setGlobalPrefix(process.env.API_PREFIX ?? 'api/v1');
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = 3000;
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix(process.env.API_PREFIX ?? "api/v1");
+  await app.listen(PORT, "0.0.0.0");
 }
 bootstrap().catch((err) => {
-  console.error('Error during bootstrap:', err);
+  console.error("Error during bootstrap:", err);
 });
