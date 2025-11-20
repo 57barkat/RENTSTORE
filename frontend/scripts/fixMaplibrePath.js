e0f6f78bd554f92a32e8db89eac76efa77f46dbc
57barkat <barkatullahkhan120136@gmail.com>
2025-11-20 09:12:02 -0500

    fixing build

--- frontend/scripts/fixMaplibrePath.js
+++ frontend/scripts/fixMaplibrePath.js
@@ -1,21 +1,34 @@
 const fs = require("fs");
 const path = require("path");
-
 // Path to your settings.gradle.kts
 // eslint-disable-next-line no-undef
-const settingsGradleKts = path.join(__dirname, "..", "android", "settings.gradle.kts");
+const gradleKts = path.join(__dirname, "..", "android", "settings.gradle.kts");
+// Path to your settings.gradle.kts
+// eslint-disable-next-line no-undef
+const gradleGroovy = path.join(__dirname, "..", "android", "settings.gradle");
 
-// Read the file
-let content = fs.readFileSync(settingsGradleKts, "utf8");
+let settingsFile = "";
 
-// Check if the patch already exists to avoid duplicates
+if (fs.existsSync(gradleKts)) {
+  settingsFile = gradleKts;
+} else if (fs.existsSync(gradleGroovy)) {
+  settingsFile = gradleGroovy;
+} else {
+  console.error(
+    "Cannot find settings.gradle.kts or settings.gradle in android folder!"
+  );
+  process.exit(1);
+}
+
+let content = fs.readFileSync(settingsFile, "utf8");
+
 if (!content.includes("FIX MAPLIBRE RELATIVE PATH")) {
-  // Add patch at the end of the file
-  content += `\n\n// ---- FIX MAPLIBRE RELATIVE PATH ----\n` +
-             `val maplibreProject = findProject(":@maplibre_maplibre-react-native")\n` +
-             `maplibreProject?.projectDir = file("../node_modules/@maplibre/maplibre-react-native/android")\n`;
+  content +=
+    `\n\n// ---- FIX MAPLIBRE RELATIVE PATH ----\n` +
+    `val maplibreProject = findProject(":@maplibre_maplibre-react-native")\n` +
+    `maplibreProject?.projectDir = file("../node_modules/@maplibre/maplibre-react-native/android")\n`;
 
-  fs.writeFileSync(settingsGradleKts, content, "utf8");
+  fs.writeFileSync(settingsFile, content, "utf8");
   console.log("Patched @maplibre maplibre-react-native path to relative.");
 } else {
   console.log("Patch already applied. Skipping.");
